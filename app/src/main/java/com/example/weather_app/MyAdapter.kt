cbb4543e81775ce1b54bf86d0469be387bfb9019
ChristianFager <christian.fager@my.metrostate.edu>
2022-02-19 11:10:18 -0600

    Assign 4

--- app/src/main/java/com/example/weather_app/MyAdapter.kt
+++ app/src/main/java/com/example/weather_app/MyAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.weather_app
 import android.annotation.SuppressLint
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +8,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import java.time.Instant
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
