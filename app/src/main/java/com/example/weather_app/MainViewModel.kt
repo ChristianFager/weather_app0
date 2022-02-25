ccb42c8392f5dfeb0a29bc05435a55cbfacc84e4
ChristianFager <christian.fager@my.metrostate.edu>
2022-02-24 20:51:45 -0600

    Assign 5

---
+++ app/src/main/java/com/example/weather_app/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.weather_app
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
+
+
+class MainViewModel @Inject constructor(private val service: Api) : ViewModel()
+{
+    val currentConditions: MutableLiveData<CurrentConditions> = MutableLiveData()
+
+
+    fun loadData() = runBlocking {
+        launch {
+            currentConditions.value = service.getCurrentConditions("98178")
+        }
+    }
+}
